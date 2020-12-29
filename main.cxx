1e52365ca7df8a91a5d35991e487daff146c46a5
虞先達 (Eric S. Yu) <ffk0716@gmail.com>
2020-12-29 01:29:54 -0800

    modified:   main.cxx

--- main.cxx
+++ main.cxx
@@ -7,33 +7,35 @@
 #include <cmath>
 #include <iostream>
 
+using namespace std;
+
 bool init_cuda()
 {
     int cuda_dev_count;
     cudaGetDeviceCount(&cuda_dev_count);
-    std::cout << "cuda device count: " << cuda_dev_count << std::endl;
+    cout << "cuda device count: " << cuda_dev_count << endl;
     if (cuda_dev_count == 0)
         return false;
 
     int dev = 0;
     cudaDeviceProp prop;
     assert(cudaGetDeviceProperties(&prop, dev) == cudaSuccess);
-    std::cout << "use GPU device " << dev << ": " << prop.name << std::endl;
-    std::cout << "SM number: " << prop.multiProcessorCount << std::endl;
-    std::cout << "Share memory per block: " << prop.sharedMemPerBlock / 1024.0 << " KB" << std::endl;
-    std::cout << "Thread per block: " << prop.maxThreadsPerBlock << std::endl;
-    std::cout << "Thread per EM: " << prop.maxThreadsPerMultiProcessor << std::endl;
+    cout << "use GPU device " << dev << ": " << prop.name << endl;
+    cout << "SM number: " << prop.multiProcessorCount << endl;
+    cout << "Share memory per block: " << prop.sharedMemPerBlock / 1024.0 << " KB" << endl;
+    cout << "Thread per block: " << prop.maxThreadsPerBlock << endl;
+    cout << "Thread per EM: " << prop.maxThreadsPerMultiProcessor << endl;
     cudaSetDevice(dev);
-    std::cout << "cuda initialized." << std::endl;
+    cout << "cuda initialized." << endl;
     return true;
 }
 
 void test_pow()
 {
     auto run = [](float x, float y) {
-        std::cout << "cuda_pow(" << x << ", " << y << ") == " << cuda_pow(x, y) << std::endl;
-        std::cout << "std::pow(" << x << ", " << y << ") == " << std::pow(x, y) << std::endl;
-        std::cout << std::endl;
+        cout << "cuda_pow(" << x << ", " << y << ") == " << cuda_pow(x, y) << endl;
+        cout << "pow(" << x << ", " << y << ") == " << pow(x, y) << endl;
+        cout << endl;
     };
     run(2, 4);
     run(-2, 3);
@@ -46,10 +48,10 @@ void test_div()
     auto run = [](float x, float y) {
         auto a = cuda_fdiv_rn(x, y);
         auto b = cuda_fdividef(x, y);
-        std::cout << "cuda_fdiv_rn(" << x << ", " << y << ") == " << cuda_fdiv_rn(x, y) << std::endl;
-        std::cout << "cuda_fdividef(" << x << ", " << y << ") == " << cuda_fdividef(x, y) << std::endl;
-        std::cout << "" << x << " / " << y << " == " << x / y << std::endl;
-        std::cout << std::endl;
+        cout << "cuda_fdiv_rn(" << x << ", " << y << ") == " << cuda_fdiv_rn(x, y) << endl;
+        cout << "cuda_fdividef(" << x << ", " << y << ") == " << cuda_fdividef(x, y) << endl;
+        cout << "" << x << " / " << y << " == " << x / y << endl;
+        cout << endl;
     };
     run(FLT_MAX, FLT_MAX);
     run(3, 2);
